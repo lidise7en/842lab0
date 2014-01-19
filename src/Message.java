815b8e857ad698c9c70dde9f30e2b47aa45398e8
Di Li <lidise7en@gmail.com>
2014-01-19 16:56:42 -0500

    add the skeleton of message and socketInfo

--- src/Message.java
+++ src/Message.java
@@ -1,4 +1,51 @@
+import java.io.Serializable;
 
-public class Message {
 
+public class Message implements Serializable {
+
+
+	private String dest;
+	private String kind;
+	private Object data;
+	private boolean duplicate;
+	private int seqNum;
+	
+	public Message(String dest, String kind, Object data) {
+		this.dest = dest;
+		this.kind = kind;
+		this.data = data;
+		this.duplicate = false;
+	}
+	// These settors are used by MessagePasser.send, not your app
+	public void set_source(String source) {
+		this.dest = source;
+	}
+	public void set_seqNum(int sequenceNumber) {
+		this.seqNum = sequenceNumber;
+	}
+	public void set_duplicate(Boolean dupe) {
+		this.duplicate = dupe;
+	}
+	// other accessors, toString, etc as needed
+	
+	public String getDest() {
+		return dest;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public Object getData() {
+		return data;
+	}
+	public boolean isDuplicate() {
+		return duplicate;
+	}
+	public int getSeqNum() {
+		return seqNum;
+	}
+	@Override
+	public String toString() {
+		return "Message [dest=" + dest + ", kind=" + kind + ", data=" + data
+				+ ", duplicate=" + duplicate + ", seqNum=" + seqNum + "]";
+	}
 }
--- src/SocketInfo.java
+++ src/SocketInfo.java
@@ -1,4 +1,29 @@
 
 public class SocketInfo {
-
+	
+	private String ipAddr;
+	private int port;
+	
+	public SocketInfo(String ip, int port) {
+		this.ipAddr = ip;
+		this.port = port;
+	}
+	public String getIpAddr() {
+		return ipAddr;
+	}
+	public void setIpAddr(String ipAddr) {
+		this.ipAddr = ipAddr;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	@Override
+	public String toString() {
+		return "SocketInfo [ipAddr=" + ipAddr + ", port=" + port + "]";
+	}
+	
 }
