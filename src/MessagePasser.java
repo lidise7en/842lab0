818a5db12b9eedc5480e88df01c87d190b2b1e75
Aditya <jaltade@cmu.edu>
2014-01-19 16:51:26 -0500

    added a skeleton structure for the class MessageParser

--- src/MessagePasser.java
+++ src/MessagePasser.java
@@ -1,4 +1,44 @@
+import java.net.Socket;
+import java.util.Map;
+import java.util.Queue;
+
 
 public class MessagePasser {
-
+	
+	private Queue<Message> sendQueue;
+	private Queue<Message> recvQueue;
+	private Map<SocketInfo, Socket> sockets;
+	String configFilename;
+	String localName;
+	
+	private enum ruleType {
+		SEND,
+		RECEIVE,
+	}
+	
+	public MessagePasser(String configuration_filename, String local_name)
+	{
+		
+	}
+	
+	void send(Message message)
+	{
+		
+	}
+	
+	Message receive()
+	{
+		return null;
+	}
+	
+	void parseConfig()
+	{
+		
+	}
+	
+	void parseRules(ruleType type)
+	{
+		
+	}
+	
 }
